406ec6c595c943c89809f1ad855b9cf3dd19661c
mli25782 <mli25782@gmail.com>
2019-02-16 00:48:03 -0500

    new v2

---
+++ HopHack2019V2/app/src/main/java/com/example/mli25782/hophack2019v2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mli25782.hophack2019v2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
